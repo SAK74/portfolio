c649d6770fc21393c31b8457b297d0e454e745b2
SAK74 <sergijks@gmail.com>
2023-08-31 21:18:54 +0200

    feat(MainPage): add description

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,12 @@
 import { Card, CardContent, CardHeader, Grid, Paper } from "@mui/material";
 import photo from "assets/photo.jpg";
 import "./main-page.css";
+import { useTranslation } from "react-i18next";
 
 export const DESCRIPTION = `   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis purus libero, a pellentesque eros cursus semper. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean blandit leo eu massa luctus, a iaculis magna placerat. Aliquam vitae purus tempor, mollis ex commodo, pharetra ipsum. Nulla facilisi. Suspendisse purus ex, pharetra dapibus libero at, pulvinar efficitur sem. Ut quis malesuada augue.`;
 
 export const MainPage = () => {
+  const { t } = useTranslation();
   return (
     <Grid
       container
@@ -34,9 +36,9 @@ export const MainPage = () => {
       </Grid>
       <Grid item xs={11} lg={6} sx={{ p: 2 }}>
         <Card>
-          <CardHeader title="About Me..." />
+          <CardHeader title={t("about.title")} />
           <CardContent sx={{ whiteSpace: "break-spaces" }}>
-            {DESCRIPTION}
+            {t("about.description").replace(":)", "\uD83D\uDE03")}
           </CardContent>
         </Card>
       </Grid>
