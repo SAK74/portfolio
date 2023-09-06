e17a0381b7c5ebb3c3b89cf80e6ad2703f4f6d94
SAK74 <sergijks@gmail.com>
2023-09-06 23:20:46 +0200

    feat: add footer

--- src/_constants.ts
+++
@@ -1,1 +0,0 @@
-export const footerHeight = "1.5rem";
---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import { GitHub } from "@mui/icons-material";
+import { Box, Link } from "@mui/material";
+
+export const Footer = () => (
+  <Box
+    component="footer"
+    sx={{
+      width: 1 / 1,
+      backgroundColor: (theme) =>
+        theme.palette.mode === "light"
+          ? theme.palette.primary.dark
+          : theme.palette.background.paper,
+      color: (theme) => theme.palette.text.secondary,
+      py: 1,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      gap: 1,
+    }}
+  >
+    &#169; SAK74
+    <Link
+      href="https://github.com/SAK74"
+      color="inherit"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <GitHub />
+    </Link>
+    2023
+  </Box>
+);
