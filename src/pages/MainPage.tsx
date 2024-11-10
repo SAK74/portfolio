780fbeb4004e3446f33c45f7bf0c6c39ce0d5d08
SAK74 <sergijks@gmail.com>
2024-11-10 22:10:20 +0100

    fix: fix responsive aboutMe page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,13 @@
 import {
+  Box,
   Card,
   CardContent,
   CardHeader,
   Grid,
   Paper,
   Stack,
+  type Theme,
+  useMediaQuery,
 } from "@mui/material";
 import photo from "assets/photo.jpg";
 import "./main-page.css";
@@ -13,6 +16,12 @@ import { MediaBar } from "components/MediaBar";
 
 export const MainPage = () => {
   const { t } = useTranslation();
+  const isLessThenMd = useMediaQuery<Theme>((theme) =>
+    theme.breakpoints.down("md")
+  );
+  const showPhotoInText = useMediaQuery<Theme>((theme) =>
+    theme.breakpoints.between("sm", "md")
+  );
   return (
     <Grid
       container
@@ -23,35 +32,55 @@ export const MainPage = () => {
         rowGap: 2,
       }}
     >
-      <Grid item xs={2} lg={2}>
-        <Stack
-          sx={{
-            "& img": {
-              width: "100%",
-              borderRadius: "inherit",
-            },
-          }}
-          spacing={6}
-        >
-          <Paper
+      {!isLessThenMd && (
+        <Grid item xs={2} md={2}>
+          <Stack
             sx={{
-              p: 0.5,
-              transform: "rotateZ(-4deg)",
+              "& img": {
+                width: "100%",
+                borderRadius: "inherit",
+              },
             }}
+            spacing={6}
           >
-            <img src={photo} alt="_photo" />
-          </Paper>
-          <MediaBar />
-        </Stack>
-      </Grid>
+            <Paper
+              sx={{
+                p: 0.5,
+                transform: "rotateZ(-4deg)",
+              }}
+            >
+              <img src={photo} alt="_photo" />
+            </Paper>
+            <MediaBar />
+          </Stack>
+        </Grid>
+      )}
 
-      <Grid item xs={11} lg={6}>
-        <Card>
-          <CardHeader title={t("about.title")} />
-          <CardContent sx={{ whiteSpace: "break-spaces" }}>
-            {t("about.description").replace(":)", "\uD83D\uDE03")}
-          </CardContent>
-        </Card>
+      <Grid item xs={11} md={6}>
+        <Box>
+          <Card>
+            <CardHeader
+              title={t("about.title")}
+              sx={{ textAlign: "center" }}
+              subheader={isLessThenMd && <MediaBar />}
+              subheaderTypographyProps={{ ...(isLessThenMd && { mt: 2 }) }}
+            />
+            <CardContent sx={{ whiteSpace: "break-spaces" }}>
+              {showPhotoInText && (
+                <Box
+                  sx={{
+                    float: "left",
+                    mr: 2,
+                    clipPath: "ellipse(40% 40%)",
+                  }}
+                >
+                  <img src={photo} alt="" width={150} />
+                </Box>
+              )}
+              {t("about.description").replace(":)", "\uD83D\uDE03")}
+            </CardContent>
+          </Card>
+        </Box>
       </Grid>
     </Grid>
   );
